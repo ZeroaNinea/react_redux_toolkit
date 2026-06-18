43eebe0e4d71acf51d78a704bd36f0be264c8e6d
Catgirl <zeroaninea@gmail.com>
2026-06-18 15:43:47 +0400

    ReactionButtons.js added

--- 02_lesson_starter/src/features/posts/PostsList.js
+++ 02_lesson_starter/src/features/posts/PostsList.js
@@ -3,6 +3,7 @@ import { selectAllPosts } from './postsSlice';
 
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -19,6 +20,7 @@ const PostsList = () => {
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
 

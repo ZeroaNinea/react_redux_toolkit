a2cafb24e5881493ff48734987ce823ad8188875
Catgirl <zeroaninea@gmail.com>
2026-06-19 23:09:53 +0400

    the initial state removed

--- 03_lesson_starter/src/features/posts/postsSlice.js
+++ 03_lesson_starter/src/features/posts/postsSlice.js
@@ -1,74 +1,63 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
+import axios from 'axios';
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    }
-]
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {
-        postAdded: {
-            reducer(state, action) {
-                state.push(action.payload)
-            },
-            prepare(title, content, userId) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        title,
-                        content,
-                        date: new Date().toISOString(),
-                        userId,
-                        reactions: {
-                            thumbsUp: 0,
-                            wow: 0,
-                            heart: 0,
-                            rocket: 0,
-                            coffee: 0
-                        }
-                    }
-                }
-            }
-        },
-        reactionAdded(state, action) {
-            const { postId, reaction } = action.payload
-            const existingPost = state.find(post => post.id === postId)
-            if (existingPost) {
-                existingPost.reactions[reaction]++
-            }
-        }
+const initialState = {
+  posts: [],
+  status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() => {
+    try {
+        const response = await axios.get(POSTS_URL)
+        return [...response.data];
+    } catch (err) {
+        return err.message;
     }
 })
 
-export const selectAllPosts = (state) => state.posts;
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.posts.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+});
 
-export const { postAdded, reactionAdded } = postsSlice.actions
+export const selectAllPosts = (state) => state.posts.posts;
 
-export default postsSlice.reducer
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;

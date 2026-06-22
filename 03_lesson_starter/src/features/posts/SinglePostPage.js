5e17c6e73cfe7d42a6757b51c2127cd736076a9b
Catgirl <zeroaninea@gmail.com>
2026-06-23 00:51:45 +0400

    the UI changed

--- 03_lesson_starter/src/features/posts/SinglePostPage.js
+++ 03_lesson_starter/src/features/posts/SinglePostPage.js
@@ -5,8 +5,10 @@ import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
 import ReactionButtons from './ReactionButtons';
 
+import { useParams } from 'react-router-dom';
+
 const SinglePostPage = () => {
-  // retrieve postId
+  const { postId } = useParams();
 
   const post = useSelector((state) => selectPostById(state, postId));
 

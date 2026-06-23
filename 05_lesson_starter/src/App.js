90fd1e5b0ec7bb5903086ab9638d245eb9e209d4
Catgirl <zeroaninea@gmail.com>
2026-06-23 18:40:41 +0400

    new routes imported

--- 05_lesson_starter/src/App.js
+++ 05_lesson_starter/src/App.js
@@ -1,15 +1,17 @@
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
-import SinglePostPage from "./features/posts/SinglePostPage";
-import EditPostForm from "./features/posts/EditPostForm";
-import Layout from "./components/Layout";
 import { Routes, Route } from 'react-router-dom';
+
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-
         <Route index element={<PostsList />} />
 
         <Route path="post">
@@ -18,6 +20,10 @@ function App() {
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
 
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
       </Route>
     </Routes>
   );
